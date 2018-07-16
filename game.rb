0e2b270094ebc86d17364e6fa976964da795118c
teeaaspoon <tommypoon36@gmail.com>
2018-07-16 21:50:30 +0000

    changed switch player code to use modulo

--- game.rb
+++ game.rb
@@ -7,9 +7,6 @@ class Game
     def start_game
         while @player1.lives > 0 && @player2.lives > 0 do
             @turn_manager.current_turn
-            if @player1.lives > 0 && @player2.lives > 0
-                @turn_manager.next_turn
-            end
         end
         if @player1.lives > @player2.lives
             puts "#{@player1.name} wins with a score of #{@player1.lives}/3"  
--- turn_manager.rb
+++ turn_manager.rb
@@ -1,13 +1,11 @@
 class TurnManager
+    @@whos_turn = 0
     def initialize(players)
         @players = players
     end
     def current_turn
-        Turn.new(@players[0]).question_string
+        Turn.new(@players[@@whos_turn%2]).question_string
         puts "#{@players[0].name}: #{@players[0].lives}/3 vs #{@players[1].name}: #{@players[1].lives}/3"
-    end
-    def next_turn
-        Turn.new(@players[1]).question_string
-        puts "#{@players[0].name}: #{@players[0].lives}/3 vs #{@players[1].name}: #{@players[1].lives}/3"
+        @@whos_turn += 1
     end
 end
